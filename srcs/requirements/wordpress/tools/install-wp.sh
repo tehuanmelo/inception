8487445cd9be520db43383f3abe895eae5d84e59
tehuan <tehuanmelo@gmail.com>
2024-03-01 23:11:39 +0400

    domain-name working

--- srcs/requirements/wordpress/tools/install-wp.sh
+++ srcs/requirements/wordpress/tools/install-wp.sh
@@ -23,6 +23,7 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
+sleep 5
 wp core install  --url="$DOMAIN_NAME" --title="$TITLE" --admin_user="$ADM_USER" --admin_password="$ADM_PASS" --admin_email="$ADM_EMAIL"
 wp user create "$WP_USER" "$WP_USER_EMAIL" --role=subscriber --user_pass="$WP_USER_PASS" 
 
